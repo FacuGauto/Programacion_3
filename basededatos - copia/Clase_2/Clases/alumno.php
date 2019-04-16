034748f0f15c0a304debf8e4b8afdc96232d0e35
FacuGauto <you@example.com>
2019-04-15 22:11:41 -0300

    dsds

---
+++ basededatos - copia/Clase_2/Clases/alumno.php
@@ -0,0 +1,58 @@
+<?php 
+include "persona.php";
+class Alumno extends persona
+{
+	public $legajo;
+	public function __construct($nombre,$edad,$dni,$legajo)
+	{
+		parent::__construct($nombre,$edad,$dni);
+		$this->legajo = $legajo;
+	}
+
+	public function jsonReturn()
+	{
+		return json_encode($this);
+	}
+
+	public function guardarAlumno($pathfile)
+	{
+		if(file_exists($pathfile))
+		{
+			$arch = fopen($pathfile,"a");
+			fwrite($arch,"\r\n".$this->nombre .",". $this->edad.",". $this->dni.",". $this->legajo);
+			fclose($arch);
+		}
+		else
+		{
+			$arch = fopen($pathfile,"w");
+			fwrite($arch,$this->nombre .",". $this->edad.",". $this->dni.",". $this->legajo);
+			fclose($arch);
+		}
+	}
+	public function guardarAlumnoJson($pathfile)
+	{
+		if(file_exists($pathfile))
+		{
+			$arch = fopen($pathfile,"a");
+			fwrite($arch,$this->jsonReturn()."\r\n");
+			fclose($arch);
+		}
+		else
+		{
+			$arch = fopen($pathfile,"w");
+			fwrite($arch,$this->jsonReturn()."\r\n");
+			fclose($arch);
+		}
+	}
+	public function mostrarAlumno()
+	{
+		echo "Nombre:" . $this->nombre;
+		echo "Edad:" . $this->edad;
+		echo "Dni:" . $this->dni;
+		echo "Legajo:" . $this->legajo;
+	}
+}
+
+
+
+?>
---
+++ basededatos - copia/Clase_2/alumno.php
@@ -0,0 +1,43 @@
+<?php 
+include "persona.php";
+class Alumno extends persona
+{
+	public $legajo;
+	/*public function __construct($nombre,$edad,$dni,$legajo)
+	{
+		parent::__construct($nombre,$edad,$dni);
+		$this->legajo = $legajo;
+	}*/
+
+	public function jsonReturn()
+	{
+		return json_encode($this);
+	}
+
+	public function guardarAlumno($pathfile)
+	{
+		if(file_exists($pathfile))
+		{
+			$arch = fopen($pathfile,"a");
+			fwrite($arch,$this);
+			fclose($arch);
+		}
+		else
+		{
+			$arch = fopen($pathfile,"w");
+			fwrite($arch,$this);
+			fclose($arch);
+		}
+	}
+	public static function TraerTodoLosAlumnos()
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("select * from alumno");
+			$consulta->execute();			
+			return $consulta->fetchAll(PDO::FETCH_CLASS, "alumno");		
+	}
+}
+
+
+
+?>
